ce6c03f2d2f63188fae2a8fde06aff54335f4380
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-08 15:47:05 +0000

    Day 7: Manual parsing

--- src/day7.rs
+++ src/day7.rs
@@ -1,44 +1,66 @@
-unsafe fn permute1(num: u64, nums: &Vec<u32>, idx: usize) -> bool {
-    let next = nums[idx] as u64;
+unsafe fn permute1(num: u64, idx: usize, st: &[u32; 16]) -> bool {
+    let next = st[idx] as u64;
     let div = num / next;
-    let rem = num % next; // please optimize this please
+    let rem = num % next;
 
     return
-        idx == 0 && num == next ||
-        idx > 0 && (
-            (rem == 0 && permute1(div, &nums, idx - 1)) ||
-            (num >= next && permute1(num - next, &nums, idx - 1))
+        idx == 1 && num == next ||
+        idx > 1 && (
+            (rem == 0 && permute1(div, idx - 1, &st)) ||
+            (num >= next && permute1(num - next, idx - 1, &st))
         );
 }
 
 pub fn part1(s: &str) -> u64 {
-    unsafe {       
-        let mut nums = vec![0u32];
-        return s.split('\n').fold(0u64, |ans, line|{
-            if line.is_empty() { return ans }
-            let mut res = 0;
-            nums.clear();
-            line.split(' ').for_each(|num| {
-                if num.ends_with(':') {
-                    //res = atoi_simd::parse_any::<u64>()
-                    res = num.replacen(':', "", 1).parse::<u64>().unwrap_unchecked();
-                } else {
-                    nums.push(num.parse::<u32>().unwrap_unchecked());
+    unsafe {
+        let mut st: [u32; 16] = [0u32; 16];
+        let b = s.as_bytes();
+        let bl = b.len();
+        let mut ans = 0u64;
+
+        let mut parsing_target  = true;
+        let mut target = 0u64;
+        let mut curr = 0u32;
+        let mut i: usize = 0;
+        for x in 0..bl {
+            match b[x] {
+                b'\n' => {
+                    st[i] = curr;
+                    if permute1(target, i, &st) { ans += target; }
+                    parsing_target = true;
+                    target = 0;
+                    curr = 0;
+                    i = 0;
+                },
+                b' ' => {
+                    st[i] = curr;
+                    i += 1;
+                    curr = 0;
+                },
+                b':' => {
+                    parsing_target = false;
+                },
+                v => {
+                    if parsing_target {
+                        target = target * 10 + (v - b'0') as u64;
+                    } else {
+                        curr = curr * 10 + (v - b'0') as u32;
+                    }
                 }
-            });
-            return ans + if permute1(res, &nums, nums.len() - 1) { res } else { 0 };
-        });
+            }
+        }
+        return ans;
     }
 }
 
-unsafe fn permute2(num: u64, nums: &Vec<u32>, idx: usize) -> bool {
-    let next = nums[idx] as u64;
-    if idx == 0 { return num == next }
+unsafe fn permute2(num: u64, idx: usize, st: &[u32; 16]) -> bool {
+    let next = st[idx] as u64;
+    if idx == 1 { return num == next }
 
     let div = num / next;
     let rem = num % next;
-    if (rem == 0 && permute2(div, &nums, idx - 1)) ||
-           (num >= next && permute2(num - next, &nums, idx - 1)) { return true; }
+    if (rem == 0 && permute2(div, idx - 1, &st)) ||
+           (num >= next && permute2(num - next, idx - 1, &st)) { return true; }
     let log = if next >= u64::pow(10, 6) {
         if next >= u64::pow(10, 10) {
             if next >= u64::pow(10, 12) {
@@ -74,24 +96,47 @@ unsafe fn permute2(num: u64, nums: &Vec<u32>, idx: usize) -> bool {
     };
     let div_ = num / log;
     let rem_ = num % log;
-    return rem_ == next && permute2(div_, &nums, idx - 1);
+    return rem_ == next && permute2(div_, idx - 1, &st);
 }
 
 pub fn part2(s: &str) -> u64 {
-    unsafe {       
-        let mut nums = vec![0u32];
-        return s.split('\n').fold(0u64, |ans, line|{
-            if line.is_empty() { return ans }
-            let mut res = 0;
-            nums.clear();
-            line.split(' ').for_each(|num| {
-                if num.ends_with(':') {
-                    res = num.replacen(':', "", 1).parse::<u64>().unwrap_unchecked();
-                } else {
-                    nums.push(num.parse::<u32>().unwrap_unchecked());
+    unsafe {
+        let mut st: [u32; 16] = [0u32; 16];
+        let b = s.as_bytes();
+        let bl = b.len();
+        let mut ans = 0u64;
+
+        let mut parsing_target  = true;
+        let mut target = 0u64;
+        let mut curr = 0u32;
+        let mut i: usize = 0;
+        for x in 0..bl {
+            match b[x] {
+                b'\n' => {
+                    st[i] = curr;
+                    if permute2(target, i, &st) { ans += target; }
+                    parsing_target = true;
+                    target = 0;
+                    curr = 0;
+                    i = 0;
+                },
+                b' ' => {
+                    st[i] = curr;
+                    i += 1;
+                    curr = 0;
+                },
+                b':' => {
+                    parsing_target = false;
+                },
+                v => {
+                    if parsing_target {
+                        target = target * 10 + (v - b'0') as u64;
+                    } else {
+                        curr = curr * 10 + (v - b'0') as u32;
+                    }
                 }
-            });
-            return ans + if permute2(res, &nums, nums.len() - 1) { res } else { 0 };
-        });
+            }
+        }
+        return ans;
     }
 }
