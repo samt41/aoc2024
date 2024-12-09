115e7eb49c54770993ef3ec896e17d86f8ad9b6b
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-09 13:59:08 +0000

    Day 8: Improve parsing

--- src/day8.rs
+++ src/day8.rs
@@ -1,22 +1,26 @@
 use core::simd::prelude::*;
 
+const W: u32 = 51;
 pub fn part1(s: &str) -> u32 {
     unsafe {
         let b = s.as_bytes();
         let bl = b.len();
         let mut antinodes = vec![0u8; bl];
         let mut nodes = vec![0u32];
-        let w = (memchr::memchr(b'\n', b).unwrap_unchecked() + 1) as u32;
-        for x in (0..bl).step_by(32) {
-            let map = u8x32::load_or_default(&b[x..usize::min(x+32, bl)]);
-            let mut mask = map.simd_ge(u8x32::splat(b'0')).to_bitmask() as u32;
+        let mut y = 0u32;
+        for x in (0..bl).step_by(W as usize) {
+            let map = u8x64::load_select_unchecked(
+                &b[x..x+W as usize],
+                mask8x64::from_bitmask((1 << (W - 1)) - 1),
+                u8x64::splat(0));
+            let mut mask = map.simd_ge(u8x64::splat(b'0')).to_bitmask() as u64;
             while mask != 0 {
-                let pos = x + mask.trailing_zeros() as usize;
-                nodes.push(((b[pos] as u32) << 16) | ((pos as u32 / w) << 8) | (pos as u32 % w));
+                let pos = mask.trailing_zeros() as usize;
+                nodes.push(((b[x + pos] as u32) << 16) | (y << 8) | pos as u32);
                 mask = mask & (mask - 1);
             }
+            y += 1;
         }
-        nodes.push(u32::MAX);
         nodes.sort_unstable();
         let mut start = 0;
         for end in 1..nodes.len() {
@@ -30,12 +34,12 @@ pub fn part1(s: &str) -> u32 {
                 let xy = ne - nc;
                 let antinode1 = nc - xy;
                 let antinode2 = ne + xy;
-                if antinode1 & 0xff < w - 1{
-                    let pos = (((antinode1 & 0xff00) * w >> 8) + (antinode1 & 0xff)) as usize;
+                if antinode1 & 0xff < W - 1{
+                    let pos = (((antinode1 & 0xff00) * W >> 8) + (antinode1 & 0xff)) as usize;
                     if pos < bl { antinodes[pos] = 1; }
                 }
-                if antinode2 & 0xff < w - 1 {
-                    let pos = (((antinode2 & 0xff00) * w >> 8) + (antinode2 & 0xff)) as usize;
+                if antinode2 & 0xff < W - 1 {
+                    let pos = (((antinode2 & 0xff00) * W >> 8) + (antinode2 & 0xff)) as usize;
                     if pos < bl { antinodes[pos] = 1; }
                 }
             }
@@ -54,17 +58,20 @@ pub fn part2(s: &str) -> u32 {
         let bl = b.len();
         let mut antinodes = vec![0u8; bl];
         let mut nodes = vec![0u32];
-        let w = (memchr::memchr(b'\n', b).unwrap_unchecked() + 1) as u32;
-        for x in (0..bl).step_by(32) {
-            let map = u8x32::load_or_default(&b[x..usize::min(x+32, bl)]);
-            let mut mask = map.simd_ge(u8x32::splat(b'0')).to_bitmask() as u32;
+        let mut y = 0u32;
+        for x in (0..bl).step_by(W as usize) {
+            let map = u8x64::load_select_unchecked(
+                &b[x..x+W as usize],
+                mask8x64::from_bitmask((1 << (W - 1)) - 1),
+                u8x64::splat(0));
+            let mut mask = map.simd_ge(u8x64::splat(b'0')).to_bitmask() as u64;
             while mask != 0 {
-                let pos = x + mask.trailing_zeros() as usize;
-                nodes.push(((b[pos] as u32) << 16) | ((pos as u32 / w) << 8) | (pos as u32 % w));
+                let pos = mask.trailing_zeros() as usize;
+                nodes.push(((b[x + pos] as u32) << 16) | (y << 8) | pos as u32);
                 mask = mask & (mask - 1);
             }
+            y += 1;
         }
-        nodes.push(u32::MAX);
         nodes.sort_unstable();
         let mut start = 0;
         for end in 1..nodes.len() {
@@ -78,16 +85,16 @@ pub fn part2(s: &str) -> u32 {
                 let xy = ne - nc;
                 let mut antinode = nc;
                 loop {
-                    let pos = (((antinode & 0xff00) * w >> 8) + (antinode & 0xff)) as usize;
-                    let valid = antinode & 0xff < w - 1 && pos < bl;
+                    let pos = (((antinode & 0xff00) * W >> 8) + (antinode & 0xff)) as usize;
+                    let valid = antinode & 0xff < W - 1 && pos < bl;
                     if valid { antinodes[pos] = 1; }
                     else { break; }
                     antinode -= xy;
                 }
                 antinode = ne;
                 loop {
-                    let pos = (((antinode & 0xff00) * w >> 8) + (antinode & 0xff)) as usize;
-                    let valid = antinode & 0xff < w - 1 && pos < bl;
+                    let pos = (((antinode & 0xff00) * W >> 8) + (antinode & 0xff)) as usize;
+                    let valid = antinode & 0xff < W - 1 && pos < bl;
                     if valid { antinodes[pos] = 1; }
                     else { break; }
                     antinode += xy;
