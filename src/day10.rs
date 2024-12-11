9ec05510e1e70638ed2d2c8cbd6510832d6c0846
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-11 15:03:45 +0000

    Day 10: U n r o l l

--- src/day10.rs
+++ src/day10.rs
@@ -4,29 +4,45 @@ pub fn part1(s: &str) -> u32 {
         let bl = b.len();
         let W = (bl as f32).sqrt() as isize;
         let D = [-W-1, 1, W+1, -1];
-        let mut stack = [0isize; 10];
-        let mut dirs = [0usize; 10];
         let mut dests = vec![0u16];
         let points = memchr::memchr_iter(b'9', b);
         return points.fold(0u32, |ans, i| {
             dests.clear();
-            let mut depth = 9;
-            stack[9] = i as isize;
-            while depth <= 9 {
-                let dp = dirs[depth];
-                if dp == 4 {
-                    dirs[depth] = 0;
-                    depth += 1;
-                    continue;
-                }
-                let next = stack[depth] + D[dp];
-                dirs[depth] = dp + 1;
-                if next >= 0 && next < bl as isize && b[next as usize] == depth as u8 + b'0' - 1 {
-                    if depth == 1 {
-                        dests.push(next as u16);
-                    } else {
-                        depth -= 1;
-                        stack[depth] = next;
+            for dir9 in 0..4 {
+                let loc8 = i as isize + D[dir9];
+                if loc8 < 0 || loc8 as usize >= bl || b[loc8 as usize] != b'8' { continue; }
+                for dir8 in 0..4 {
+                    let loc7 = loc8 + D[dir8];
+                    if loc7 < 0 || loc7 as usize >= bl || b[loc7 as usize] != b'7' { continue; }
+                    for dir7 in 0..4 {
+                        let loc6 = loc7 + D[dir7];
+                        if loc6 < 0 || loc6 as usize >= bl || b[loc6 as usize] != b'6' { continue; }
+                        for dir6 in 0..4 {
+                            let loc5 = loc6 + D[dir6];
+                            if loc5 < 0 || loc5 as usize >= bl || b[loc5 as usize] != b'5' { continue; }
+                            for dir5 in 0..4 {
+                                let loc4 = loc5 + D[dir5];
+                                if loc4 < 0 || loc4 as usize >= bl || b[loc4 as usize] != b'4' { continue; }
+                                for dir4 in 0..4 {
+                                    let loc3 = loc4 + D[dir4];
+                                    if loc3 < 0 || loc3 as usize >= bl || b[loc3 as usize] != b'3' { continue; }
+                                    for dir3 in 0..4 {
+                                        let loc2 = loc3 + D[dir3];
+                                        if loc2 < 0 || loc2 as usize >= bl || b[loc2 as usize] != b'2' { continue; }
+                                        for dir2 in 0..4 {
+                                            let loc1 = loc2 + D[dir2];
+                                            if loc1 < 0 || loc1 as usize >= bl || b[loc1 as usize] != b'1' { continue; }
+                                            for dir1 in 0..4 {
+                                                let loc0 = loc1 + D[dir1];
+                                                if loc0 >= 0 && (loc0 as usize) < bl && b[loc0 as usize] == b'0' {
+                                                    dests.push(loc0 as u16);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -43,32 +59,45 @@ pub fn part2(s: &str) -> u32 {
         let bl = b.len();
         let W = (bl as f32).sqrt() as isize;
         let D = [-W-1, 1, W+1, -1];
-        let mut stack = [0isize; 10];
-        let mut dirs = [0usize; 10];
         let points = memchr::memchr_iter(b'9', b);
-        return points.fold(0u32, |ans, i| {
-            let mut score = 0u32;
-            let mut depth = 9;
-            stack[9] = i as isize;
-            while depth <= 9 {
-                let dp = dirs[depth];
-                if dp == 4 {
-                    dirs[depth] = 0;
-                    depth += 1;
-                    continue;
-                }
-                let next = stack[depth] + D[dp];
-                dirs[depth] = dp + 1;
-                if next >= 0 && next < bl as isize && b[next as usize] == depth as u8 + b'0' - 1 {
-                    if depth == 1 {
-                        score += 1;
-                    } else {
-                        depth -= 1;
-                        stack[depth] = next;
+        return points.fold(0u32, |mut ans, i| {
+            for dir9 in 0..4 {
+                let loc8 = i as isize + D[dir9];
+                if loc8 < 0 || loc8 as usize >= bl || b[loc8 as usize] != b'8' { continue; }
+                for dir8 in 0..4 {
+                    let loc7 = loc8 + D[dir8];
+                    if loc7 < 0 || loc7 as usize >= bl || b[loc7 as usize] != b'7' { continue; }
+                    for dir7 in 0..4 {
+                        let loc6 = loc7 + D[dir7];
+                        if loc6 < 0 || loc6 as usize >= bl || b[loc6 as usize] != b'6' { continue; }
+                        for dir6 in 0..4 {
+                            let loc5 = loc6 + D[dir6];
+                            if loc5 < 0 || loc5 as usize >= bl || b[loc5 as usize] != b'5' { continue; }
+                            for dir5 in 0..4 {
+                                let loc4 = loc5 + D[dir5];
+                                if loc4 < 0 || loc4 as usize >= bl || b[loc4 as usize] != b'4' { continue; }
+                                for dir4 in 0..4 {
+                                    let loc3 = loc4 + D[dir4];
+                                    if loc3 < 0 || loc3 as usize >= bl || b[loc3 as usize] != b'3' { continue; }
+                                    for dir3 in 0..4 {
+                                        let loc2 = loc3 + D[dir3];
+                                        if loc2 < 0 || loc2 as usize >= bl || b[loc2 as usize] != b'2' { continue; }
+                                        for dir2 in 0..4 {
+                                            let loc1 = loc2 + D[dir2];
+                                            if loc1 < 0 || loc1 as usize >= bl || b[loc1 as usize] != b'1' { continue; }
+                                            for dir1 in 0..4 {
+                                                let loc0 = loc1 + D[dir1];
+                                                ans += (loc0 >= 0 && (loc0 as usize) < bl && b[loc0 as usize] == b'0') as u32;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
-            return ans + score;
+            return ans as u32;
         });
     }
 }
