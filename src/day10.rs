ad8998bcf003384da8afad456182a78ddac3f2fc
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-11 16:24:34 +0000

    Day 10: Silence warnings

--- src/day10.rs
+++ src/day10.rs
@@ -2,37 +2,37 @@ pub fn part1(s: &str) -> u32 {
     unsafe {
         let b = s.as_bytes();
         let bl = b.len();
-        let W = (bl as f32).sqrt() as usize;
-        let D = [(!(W+1))+1, 1, W+1, (!1)+1];
+        let w = (bl as f32).sqrt() as usize;
+        let d = [(!(w+1))+1, 1, w+1, (!1)+1];
         let mut vis = [0u64; 128];
         let points = memchr::memchr_iter(b'9', b);
         return points.fold(0u32, |mut ans, i| {
             for dir9 in 0..4 {
-                let loc8 = i.wrapping_add(*D.get_unchecked(dir9));
+                let loc8 = i.wrapping_add(*d.get_unchecked(dir9));
                 if loc8 >= bl || *b.get_unchecked(loc8) != b'8' { continue; }
                 for dir8 in 0..4 {
-                    let loc7 = loc8.wrapping_add(*D.get_unchecked(dir8));
+                    let loc7 = loc8.wrapping_add(*d.get_unchecked(dir8));
                     if loc7 >= bl || *b.get_unchecked(loc7) != b'7' { continue; }
                     for dir7 in 0..4 {
-                        let loc6 = loc7.wrapping_add(*D.get_unchecked(dir7));
+                        let loc6 = loc7.wrapping_add(*d.get_unchecked(dir7));
                         if loc6 >= bl || *b.get_unchecked(loc6) != b'6' { continue; }
                         for dir6 in 0..4 {
-                            let loc5 = loc6.wrapping_add(*D.get_unchecked(dir6));
+                            let loc5 = loc6.wrapping_add(*d.get_unchecked(dir6));
                             if loc5 >= bl || *b.get_unchecked(loc5) != b'5' { continue; }
                             for dir5 in 0..4 {
-                                let loc4 = loc5.wrapping_add(*D.get_unchecked(dir5));
+                                let loc4 = loc5.wrapping_add(*d.get_unchecked(dir5));
                                 if loc4 >= bl || *b.get_unchecked(loc4) != b'4' { continue; }
                                 for dir4 in 0..4 {
-                                    let loc3 = loc4.wrapping_add(*D.get_unchecked(dir4));
+                                    let loc3 = loc4.wrapping_add(*d.get_unchecked(dir4));
                                     if loc3 >= bl || *b.get_unchecked(loc3) != b'3' { continue; }
                                     for dir3 in 0..4 {
-                                        let loc2 = loc3.wrapping_add(*D.get_unchecked(dir3));
+                                        let loc2 = loc3.wrapping_add(*d.get_unchecked(dir3));
                                         if loc2 >= bl || *b.get_unchecked(loc2) != b'2' { continue; }
                                         for dir2 in 0..4 {
-                                            let loc1 = loc2.wrapping_add(*D.get_unchecked(dir2));
+                                            let loc1 = loc2.wrapping_add(*d.get_unchecked(dir2));
                                             if loc1 >= bl || *b.get_unchecked(loc1) != b'1' { continue; }
                                             for dir1 in 0..4 {
-                                                let loc0 = loc1.wrapping_add(*D.get_unchecked(dir1));
+                                                let loc0 = loc1.wrapping_add(*d.get_unchecked(dir1));
                                                 if loc0 < bl && b[loc0] == b'0' {
                                                     // let prev = vis[loc0 >> 6];
                                                     vis[loc0 >> 6] |= 1 << (loc0 & 63);
@@ -60,36 +60,36 @@ pub fn part2(s: &str) -> u32 {
     unsafe {
         let b = s.as_bytes();
         let bl = b.len();
-        let W = (bl as f32).sqrt() as usize;
-        let D = [(!(W+1))+1, 1, W+1, (!1)+1];
+        let w = (bl as f32).sqrt() as usize;
+        let d = [(!(w+1))+1, 1, w+1, (!1)+1];
         let points = memchr::memchr_iter(b'9', b);
         return points.fold(0u32, |mut ans, i| {
             for dir9 in 0..4 {
-                let loc8 = i.wrapping_add(*D.get_unchecked(dir9));
+                let loc8 = i.wrapping_add(*d.get_unchecked(dir9));
                 if loc8 >= bl || *b.get_unchecked(loc8) != b'8' { continue; }
                 for dir8 in 0..4 {
-                    let loc7 = loc8.wrapping_add(*D.get_unchecked(dir8));
+                    let loc7 = loc8.wrapping_add(*d.get_unchecked(dir8));
                     if loc7 >= bl || *b.get_unchecked(loc7) != b'7' { continue; }
                     for dir7 in 0..4 {
-                        let loc6 = loc7.wrapping_add(*D.get_unchecked(dir7));
+                        let loc6 = loc7.wrapping_add(*d.get_unchecked(dir7));
                         if loc6 >= bl || *b.get_unchecked(loc6) != b'6' { continue; }
                         for dir6 in 0..4 {
-                            let loc5 = loc6.wrapping_add(*D.get_unchecked(dir6));
+                            let loc5 = loc6.wrapping_add(*d.get_unchecked(dir6));
                             if loc5 >= bl || *b.get_unchecked(loc5) != b'5' { continue; }
                             for dir5 in 0..4 {
-                                let loc4 = loc5.wrapping_add(*D.get_unchecked(dir5));
+                                let loc4 = loc5.wrapping_add(*d.get_unchecked(dir5));
                                 if loc4 >= bl || *b.get_unchecked(loc4) != b'4' { continue; }
                                 for dir4 in 0..4 {
-                                    let loc3 = loc4.wrapping_add(*D.get_unchecked(dir4));
+                                    let loc3 = loc4.wrapping_add(*d.get_unchecked(dir4));
                                     if loc3 >= bl || *b.get_unchecked(loc3) != b'3' { continue; }
                                     for dir3 in 0..4 {
-                                        let loc2 = loc3.wrapping_add(*D.get_unchecked(dir3));
+                                        let loc2 = loc3.wrapping_add(*d.get_unchecked(dir3));
                                         if loc2 >= bl || *b.get_unchecked(loc2) != b'2' { continue; }
                                         for dir2 in 0..4 {
-                                            let loc1 = loc2.wrapping_add(*D.get_unchecked(dir2));
+                                            let loc1 = loc2.wrapping_add(*d.get_unchecked(dir2));
                                             if loc1 >= bl || *b.get_unchecked(loc1) != b'1' { continue; }
                                             for dir1 in 0..4 {
-                                                let loc0 = loc1.wrapping_add(*D.get_unchecked(dir1));
+                                                let loc0 = loc1.wrapping_add(*d.get_unchecked(dir1));
                                                 ans += (loc0 < bl && *b.get_unchecked(loc0) == b'0') as u32;
                                             }
                                         }
