8470fdfe06ad597897d097206d411641ee5bf53c
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-13 13:59:13 +0000

    Day 11: Use flat array in front of hashmap
    I fell asleep and forgot to push

--- src/day11.rs
+++ src/day11.rs
@@ -1,10 +1,13 @@
 use std::mem::MaybeUninit;
+use std::simd::prelude::*;
 
 use intmap::{ IntMap, Entry };
 
 static mut init: bool = false;
 static mut map1: MaybeUninit<IntMap<u64, u64>> = MaybeUninit::uninit();
 static mut map2: MaybeUninit<IntMap<u64, u64>> = MaybeUninit::uninit();
+const L: usize = 960;
+
 unsafe fn _init1() {
     map1.write(IntMap::with_capacity(512));
     map2.write(IntMap::with_capacity(512));
@@ -17,72 +20,123 @@ unsafe fn _init2() {
     init = true;
 }
 
-unsafe fn _loop(iters: u32) -> u64 {
+unsafe fn _loop(iters: u32, mem: &mut [u64; L * 2]) -> u64 {
     let m1 = map1.assume_init_mut();
     let m2 = map2.assume_init_mut();
     for _ in 0..iters/2 {
         m2.clear();
-        let zero = m1.remove(0);
-        if zero.is_some() {
-            m2.insert(1, zero.unwrap_unchecked());
+        mem[L..].fill(0);
+        mem[L + 1] = mem[0];
+        for i in 1..10 {
+            if mem[i] > 0 {
+                m2.insert(i as u64 * 2024, mem[i]);
+            }
+        }
+        for i in 10..100 {
+            let v = mem[i];
+            mem[L + i / 10] += v;
+            mem[L + i % 10] += v;
+        }
+        for i in 100..L {
+            if mem[i] > 0 {
+                m2.insert(i as u64 * 2024, mem[i]);
+            }
         }
         m1.drain().for_each(|(k, v)| {
             let lg = k.ilog10();
             if lg & 1 == 1 {
                 let md = 10u64.pow((lg + 1) >> 1);
                 let (x, y) = (k / md, k % md);
-                *match m2.entry(x) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
-                *match m2.entry(y) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
+                if x < L as u64 { mem[L + x as usize] += v }
+                else {
+                    *match m2.entry(x) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
+                if y < L as u64 { mem[L + y as usize] += v }
+                else {
+                    *match m2.entry(y) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
             } else {
-                *match m2.entry(k * 2024) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
+                let x = k * 2024;
+                if x < L as u64 { mem[L + x as usize] += v }
+                else {
+                    *match m2.entry(k * 2024) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
             }
         });
 
         m1.clear();
-        let zero = m2.remove(0);
-        if zero.is_some() {
-            m1.insert(1, zero.unwrap_unchecked());
+        mem[..L].fill(0);
+        mem[1] = mem[L];
+        for i in 1..10 {
+            if mem[L + i] > 0 {
+                m1.insert(i as u64 * 2024, mem[L + i]);
+            }
+        }
+        for i in 10..100 {
+            let v = mem[L + i];
+            mem[i / 10] += v;
+            mem[i % 10] += v;
+        }
+        for i in 100..L {
+            if mem[L + i] > 0 {
+                m1.insert(i as u64 * 2024, mem[L + i]);
+            }
         }
         m2.drain().for_each(|(k, v)| {
             let lg = k.ilog10();
             if lg & 1 == 1 {
                 let md = 10u64.pow((lg + 1) >> 1);
                 let (x, y) = (k / md, k % md);
-                *match m1.entry(x) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
-                *match m1.entry(y) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
+                if x < L as u64 { mem[x as usize] += v }
+                else {
+                    *match m1.entry(x) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
+                if y < L as u64 { mem[y as usize] += v }
+                else {
+                    *match m1.entry(y) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
             } else {
-                *match m1.entry(k * 2024) {
-                    Entry::Occupied(entry) => entry.into_mut(),
-                    Entry::Vacant(entry) => entry.insert(0),
-                } += v;
+                let x = k * 2024;
+                if x < L as u64 { mem[x as usize] += v }
+                else {
+                    *match m1.entry(k * 2024) {
+                        Entry::Occupied(entry) => entry.into_mut(),
+                        Entry::Vacant(entry) => entry.insert(0),
+                    } += v;
+                }
             }
         });
     }
-    let ans = m1.remove(0).unwrap_or(0) + 
-    m1.drain().fold(0u64, |i, (k, v)| {
+    let mut ans = 0u64;
+    for i in (0..L).step_by(64) {
+        ans += u64x64::load_select_unchecked(&mem[i..i+64], mask64x64::splat(true), u64x64::splat(0)).reduce_sum();
+    }
+    ans += u64x64::load_select_unchecked(&mem[10..74], mask64x64::splat(true), u64x64::splat(0)).reduce_sum();
+    ans += u64x32::load_select_unchecked(&mem[74..100], mask64x32::from_bitmask((1 << 26) - 1), u64x32::splat(0)).reduce_sum();
+    return m1.drain().fold(ans, |i, (k, v)| {
         let lg = k.ilog10();
         return i + (v << (lg & 1));
     });
-    return ans;
 }
 
 pub fn part1(s: &str) -> u32 {
     unsafe {
+        let scratch = &mut [0u64; L * 2];
         if !init { _init1() }
         let m = map1.assume_init_mut();
         m.clear();
@@ -92,13 +146,16 @@ pub fn part1(s: &str) -> u32 {
             let res = atoi_simd::parse_any_pos::<u64>(&b[i..]);
             match res {
                 Ok((v, l)) => {
-                    *match m.entry(v) {
-                        Entry::Occupied(entry) => entry.into_mut(),
-                        Entry::Vacant(entry) => entry.insert(0),
-                    } += 1;
+                    if v < L as u64 { scratch[v as usize] += 1; }
+                    else {
+                        *match m.entry(v) {
+                            Entry::Occupied(entry) => entry.into_mut(),
+                            Entry::Vacant(entry) => entry.insert(0),
+                        } += 1;
+                    }
                     i += l + 1;
                 },
-                Err(_) => return _loop(25) as u32
+                Err(_) => return _loop(25, scratch) as u32
             }
         }
     }
@@ -107,6 +164,7 @@ pub fn part1(s: &str) -> u32 {
 pub fn part2(s: &str) -> u64 {
     unsafe {
         if !init { _init2() }
+        let scratch = &mut [0u64; L * 2];
         let m = map1.assume_init_mut();
         m.clear();
         let b = s.as_bytes();
@@ -115,13 +173,16 @@ pub fn part2(s: &str) -> u64 {
             let res = atoi_simd::parse_any_pos::<u64>(&b[i..]);
             match res {
                 Ok((v, l)) => {
-                    *match m.entry(v) {
-                        Entry::Occupied(entry) => entry.into_mut(),
-                        Entry::Vacant(entry) => entry.insert(0),
-                    } += 1;
+                    if v < L as u64 { scratch[v as usize] += 1; }
+                    else {
+                        *match m.entry(v) {
+                            Entry::Occupied(entry) => entry.into_mut(),
+                            Entry::Vacant(entry) => entry.insert(0),
+                        } += 1;
+                    }
                     i += l + 1;
                 },
-                Err(_) => return _loop(75)
+                Err(_) => return _loop(75, scratch)
             }
         }
     }
