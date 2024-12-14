6484658eed543e48c8acccc1813e92c413a19242
samt41 <135221808+samt41@users.noreply.github.com>
2024-12-14 14:35:33 +0000

    Day 13: Tiny flow changes

--- src/day13.rs
+++ src/day13.rs
@@ -37,31 +37,37 @@ static GCD: [GcdObj; 10000] = {
     arr
 };
 
-// there are 320 test cases.
-// each test case needs 4 input numbers. so we can do them in chunks of 16
 unsafe fn calc1(data: u8x64, targets: u16x32) -> u32 {
-    let x1 = simd_swizzle!(data, [0, 4,  8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60]).cast::<i32>();
-    let y1 = simd_swizzle!(data, [1, 5,  9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61]).cast::<i32>();
-    let x2 = simd_swizzle!(data, [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62]).cast::<i32>();
-    let y2 = simd_swizzle!(data, [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63]).cast::<i32>();
-    let X = simd_swizzle!(targets, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]).cast::<i32>();
-    let Y = simd_swizzle!(targets, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]).cast::<i32>();
+    let zeros = i32x16::splat(0);
+    #[cfg(debug_assertions)]
+    let ones = i32x16::splat(1);
+    let hundredsu = u32x16::splat(100);
+    let x1 = data.resize::<16>(0).cast::<i32>();
+    let y1 = data.rotate_elements_left::<16>().resize::<16>(0).cast::<i32>();
+    let x2 = data.rotate_elements_left::<32>().resize::<16>(0).cast::<i32>();
+    let y2 = data.rotate_elements_left::<48>().resize::<16>(0).cast::<i32>();
+    let X = targets.resize::<16>(0).cast::<i32>();
+    let Y = targets.rotate_elements_left::<16>().resize::<16>(0).cast::<i32>();
     let det = x1 * y2 - x2 * y1;
-    let sol1 = det.simd_ne(i32x16::splat(0));
+    let sol1 = det.simd_ne(zeros);
     // unique solution
     let solA = X * y2 - Y * x2;
     let solB = Y * x1 - X * y1;
     // can we just unsafe divide by 0? H
-    let A_int = solA / simd_select(sol1.to_int(), det, i32x16::splat(1));
-    let B_int = solB / simd_select(sol1.to_int(), det, i32x16::splat(1));
-    //let sol1_is_integer = (solA % simd_select(sol1.to_int(), det, u32x16::splat(1))).simd_eq(u32x16::splat(0)) &
-    //    (solB % simd_select(sol1.to_int(), det, u32x16::splat(1))).simd_eq(u32x16::splat(0));
+    #[cfg(not(debug_assertions))]
+    let A_int = solA / det;
+    #[cfg(not(debug_assertions))]
+    let B_int = solB / det;
+    #[cfg(debug_assertions)]
+    let A_int = solA / simd_select(sol1.to_int(), det, ones);
+    #[cfg(debug_assertions)]
+    let B_int = solB / simd_select(sol1.to_int(), det, ones);
     let sol1_is_integer = (A_int * det).simd_eq(solA) & (B_int * det).simd_eq(solB);
-    let sol1_in_range = A_int.cast::<u32>().simd_le(u32x16::splat(100)) & B_int.cast::<u32>().simd_le(u32x16::splat(100));
+    let sol1_in_range = A_int.cast::<u32>().simd_le(hundredsu) & B_int.cast::<u32>().simd_le(hundredsu);
     let sol1_valid = (sol1_is_integer & sol1_in_range).to_int();
 
-    let A1 = simd_select(sol1_valid, A_int, i32x16::splat(0));
-    let B1 = simd_select(sol1_valid, B_int, i32x16::splat(0));
+    let A1 = simd_select(sol1_valid, A_int, zeros);
+    let B1 = simd_select(sol1_valid, B_int, zeros);
 
     let mut ans = (A1 * i32x16::splat(3) + B1).reduce_sum() as u32;
 
@@ -69,7 +75,7 @@ unsafe fn calc1(data: u8x64, targets: u16x32) -> u32 {
         return ans;
     }
     // no solution
-    let sol0 = (x1 * Y - y1 * X).simd_ne(i32x16::splat(0));
+    let sol0 = (x1 * Y - y1 * X).simd_ne(zeros);
     // infinite solutions (collinear)
     let mut solinf = (!sol0 & !sol1).to_bitmask();
     if solinf == 0 { return ans; }
@@ -112,53 +118,60 @@ pub fn part1(s: &str) -> u32 {
         let mut ptr = 0;
         let mut buf = [0u8; 64];
         let mut buf_target = [0u16; 32];
-        let mut buf_ptr = 0;
-        let mut buf_target_ptr = 0;
         let mut ans = 0u32;
+        let mul = u8x8::from_array([10, 1, 10, 1, 10, 1, 10, 1]);
+        let buf_dest = usizex4::from_array([0, 16, 32, 48]);
         while ptr < bl {
-            ptr += 12;
-            let stuff= u8x32::load_select_unchecked(&b[ptr..ptr + 32], mask8x32::splat(true), u8x32::splat(0));
-            let stuff2 = simd_swizzle!(stuff, [0, 1, 6, 7, 21, 22, 27, 28]) - u8x8::splat(b'0');
-            let stuff3 = simd_swizzle!(stuff2, [0, 2, 4, 6]) * u8x4::splat(10);
-            let stuff4 = stuff3 + simd_swizzle!(stuff2, [1, 3, 5, 7]);
-            stuff4.store_select_unchecked(&mut buf[buf_ptr..buf_ptr+4], mask8x4::splat(true));
-            ptr += 39;
-            let (num1, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
-            ptr += len + 4;
-            let (num2, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
-            ptr += len + 2;
-            buf_target[buf_target_ptr] = num1;
-            buf_target[buf_target_ptr + 1] = num2;
-            if buf_ptr == 60 {
-                ans += calc1(buf.into(), buf_target.into());
+            for buf_ptr in 0..16 {
+                ptr += 12;
+                let stuff= u8x32::load_select_unchecked(&b[ptr..ptr + 32], mask8x32::splat(true), u8x32::splat(0));
+                let stuff2 = simd_swizzle!(stuff, [0, 1, 6, 7, 21, 22, 27, 28]) * mul;
+                let stuff4 = simd_swizzle!(stuff2, [0, 2, 4, 6]) + simd_swizzle!(stuff2, [1, 3, 5, 7]) - u8x4::splat(b'0'.wrapping_mul(11));
+                stuff4.scatter_select_unchecked(&mut buf[buf_ptr..], masksizex4::splat(true), buf_dest);
+                ptr += 39;
+                let (num1, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
+                ptr += len + 4;
+                let (num2, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
+                ptr += len + 2;
+                buf_target[buf_ptr] = num1;
+                buf_target[16 + buf_ptr] = num2;
             }
-            buf_ptr = (buf_ptr + 4) & 63;
-            buf_target_ptr = (buf_target_ptr + 2) & 31;
+            ans += calc1(buf.into(), buf_target.into());
         }
         return ans;
     }
 }
 
 unsafe fn calc2(data: u8x64, _targets: u16x32) -> u64 {
+    let zeros = i64x16::splat(0);
+    #[cfg(debug_assertions)]
+    let ones = i64x16::splat(1);
     let targets = _targets.cast::<i64>() + i64x32::splat(10000000000000);
-    let x1 = simd_swizzle!(data, [0, 4,  8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60]).cast::<i64>();
-    let y1 = simd_swizzle!(data, [1, 5,  9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61]).cast::<i64>();
-    let x2 = simd_swizzle!(data, [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62]).cast::<i64>();
-    let y2 = simd_swizzle!(data, [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63]).cast::<i64>();
-    let X = simd_swizzle!(targets, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]);
-    let Y = simd_swizzle!(targets, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]);
+    let x1 = data.resize::<16>(0).cast::<i64>();
+    let y1 = data.rotate_elements_left::<16>().resize::<16>(0).cast::<i64>();
+    let x2 = data.rotate_elements_left::<32>().resize::<16>(0).cast::<i64>();
+    let y2 = data.rotate_elements_left::<48>().resize::<16>(0).cast::<i64>();
+    let X = targets.resize::<16>(0).cast::<i64>();
+    let Y = targets.rotate_elements_left::<16>().resize::<16>(0).cast::<i64>();
+
     let det = x1 * y2 - x2 * y1;
-    let sol1 = det.simd_ne(i64x16::splat(0));
+    let sol1 = det.simd_ne(zeros);
     // unique solution
     let solA = X * y2 - Y * x2;
     let solB = Y * x1 - X * y1;
 
-    let A_int = solA / simd_select(sol1.to_int(), det, i64x16::splat(1));
-    let B_int = solB / simd_select(sol1.to_int(), det, i64x16::splat(1));
+    #[cfg(not(debug_assertions))]
+    let A_int = solA / det;
+    #[cfg(not(debug_assertions))]
+    let B_int = solB / det;
+    #[cfg(debug_assertions)]
+    let A_int = solA / simd_select(sol1.to_int(), det, ones);
+    #[cfg(debug_assertions)]
+    let B_int = solB / simd_select(sol1.to_int(), det, ones);
     let sol1_valid = ((A_int * det).simd_eq(solA) & (B_int * det).simd_eq(solB)).to_int();
 
-    let A1 = simd_select(sol1_valid, A_int, i64x16::splat(0));
-    let B1 = simd_select(sol1_valid, B_int, i64x16::splat(0));
+    let A1 = simd_select(sol1_valid, A_int, zeros);
+    let B1 = simd_select(sol1_valid, B_int, zeros);
 
     let mut ans = (A1 * i64x16::splat(3) + B1).reduce_sum() as u64;
 
@@ -166,7 +179,7 @@ unsafe fn calc2(data: u8x64, _targets: u16x32) -> u64 {
         return ans;
     }
     // no solution
-    let sol0 = (x1 * Y - y1 * X).simd_ne(i64x16::splat(0));
+    let sol0 = (x1 * Y - y1 * X).simd_ne(zeros);
     // infinite solutions (collinear)
     let mut solinf = (!sol0 & !sol1).to_bitmask();
     if solinf == 0 { return ans; }
@@ -207,28 +220,25 @@ pub fn part2(s: &str) -> u64 {
         let mut ptr = 0;
         let mut buf = [0u8; 64];
         let mut buf_target = [0u16; 32];
-        let mut buf_ptr = 0;
-        let mut buf_target_ptr = 0;
         let mut ans = 0u64;
+        let mul = u8x8::from_array([10, 1, 10, 1, 10, 1, 10, 1]);
+        let buf_dest = usizex4::from_array([0, 16, 32, 48]);
         while ptr < bl {
-            ptr += 12;
-            let stuff= u8x32::load_select_unchecked(&b[ptr..ptr + 32], mask8x32::splat(true), u8x32::splat(0));
-            let stuff2 = simd_swizzle!(stuff, [0, 1, 6, 7, 21, 22, 27, 28]) - u8x8::splat(b'0');
-            let stuff3 = simd_swizzle!(stuff2, [0, 2, 4, 6]) * u8x4::splat(10);
-            let stuff4 = stuff3 + simd_swizzle!(stuff2, [1, 3, 5, 7]);
-            stuff4.store_select_unchecked(&mut buf[buf_ptr..buf_ptr+4], mask8x4::splat(true));
-            ptr += 39;
-            let (num1, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
-            ptr += len + 4;
-            let (num2, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
-            ptr += len + 2;
-            buf_target[buf_target_ptr] = num1;
-            buf_target[buf_target_ptr + 1] = num2;
-            if buf_ptr == 60 {
-                ans += calc2(buf.into(), buf_target.into());
+            for buf_ptr in 0..16 {
+                ptr += 12;
+                let stuff= u8x32::load_select_unchecked(&b[ptr..ptr + 32], mask8x32::splat(true), u8x32::splat(0));
+                let stuff2 = simd_swizzle!(stuff, [0, 1, 6, 7, 21, 22, 27, 28]) * mul;
+                let stuff4 = simd_swizzle!(stuff2, [0, 2, 4, 6]) + simd_swizzle!(stuff2, [1, 3, 5, 7]) - u8x4::splat(b'0'.wrapping_mul(11));
+                stuff4.scatter_select_unchecked(&mut buf[buf_ptr..], masksizex4::splat(true), buf_dest);
+                ptr += 39;
+                let (num1, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
+                ptr += len + 4;
+                let (num2, len) = atoi_simd::parse_any_pos::<u16>(&b[ptr..]).unwrap_unchecked();
+                ptr += len + 2;
+                buf_target[buf_ptr] = num1;
+                buf_target[16 + buf_ptr] = num2;
             }
-            buf_ptr = (buf_ptr + 4) & 63;
-            buf_target_ptr = (buf_target_ptr + 2) & 31;
+            ans += calc2(buf.into(), buf_target.into());
         }
         return ans;
     }
